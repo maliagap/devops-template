142d26293ba33868cdff376eb109c21838858961
Miguel Augusto Aliaga Pantoja <maliaga.pantoja@gmail.com>
2025-03-25 15:39:12 -0500

    Update terraform.mk

--- devops/scripts/mkfiles/terraform.mk
+++ devops/scripts/mkfiles/terraform.mk
@@ -7,6 +7,6 @@ apply:
 validate:
 	@./scripts/bash/terraform.sh script_terraform_validate
 fmt:
-        @./scripts/bash/terraform.sh script_terraform_fmt
+	@./scripts/bash/terraform.sh script_terraform_fmt
 lint:
-        @./scripts/bash/terraform.sh script_terraform_lint
+	@./scripts/bash/terraform.sh script_terraform_lint
